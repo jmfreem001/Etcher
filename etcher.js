0b0c59790308143454807b05a1ac0da8e7efcfb6
Josh Freeman <jmfreem001@gmail.com>
2019-06-11 22:08:58 -0500

    Initial commit

---
+++ etcher.js
@@ -0,0 +1,18 @@
+
+let dimension =  16;
+let container = document.querySelector(".container";)
+for (let i = 0; i < dimension; i++){
+    //for j < dimension i ++
+    //create element
+    //add class cell
+    // append as child to container. 
+
+    //Need to set max width to dimension and then start a new row
+
+}
+
+// Event listener "onclick"for "#shaker" to reset grid 
+  //prompt for squares per side of new sketch area higher provides more detail. 
+  // (Optionally) Add a shake animation
+
+// Event listener for each div "onhover" change background color to black. 
